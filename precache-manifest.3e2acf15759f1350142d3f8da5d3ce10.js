4694a4366d500c6193bf0073c7961e2cc7aefd2d
Jesus Rojas <jesusarvelais1000@hotmail.com>
2020-09-25 23:01:16 -0700

    Updates

---
+++ precache-manifest.3e2acf15759f1350142d3f8da5d3ce10.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e918922029e5c73ba9324dd899850c1",
+    "url": "/tuki/index.html"
+  },
+  {
+    "revision": "d4577e1621c3df997f3f",
+    "url": "/tuki/static/css/main.f23d5e03.chunk.css"
+  },
+  {
+    "revision": "1e7b2d6e3b9d61c1029e",
+    "url": "/tuki/static/js/2.4d8c3236.chunk.js"
+  },
+  {
+    "revision": "fbe06bcc249d3af1b96e1e74b0ad1f88",
+    "url": "/tuki/static/js/2.4d8c3236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4577e1621c3df997f3f",
+    "url": "/tuki/static/js/main.92798220.chunk.js"
+  },
+  {
+    "revision": "d71b582e3fef6f0319d7",
+    "url": "/tuki/static/js/runtime-main.75522ff8.js"
+  },
+  {
+    "revision": "30c763a6180f40c40ec3ab4c0098b95f",
+    "url": "/tuki/static/media/Tint Color.30c763a6.txt"
+  },
+  {
+    "revision": "8418d1929b698b1f71ea1d74223d6d1e",
+    "url": "/tuki/static/media/ale2.8418d192.png"
+  },
+  {
+    "revision": "d7fea702c843b762d2c44593e157fd45",
+    "url": "/tuki/static/media/background-large.d7fea702.jpg"
+  },
+  {
+    "revision": "083da8cf7d029e085d2751329a386eda",
+    "url": "/tuki/static/media/daniel_millan4.083da8cf.png"
+  },
+  {
+    "revision": "6e1daa2c8dc607ec640180f2b8bbf018",
+    "url": "/tuki/static/media/david_gamboa4.6e1daa2c.png"
+  },
+  {
+    "revision": "0b7360b986d353ae33676a46b3b21b61",
+    "url": "/tuki/static/media/fabrizio_brandani2.0b7360b9.png"
+  },
+  {
+    "revision": "6a632f5315003d7f61040abf70e69742",
+    "url": "/tuki/static/media/octavio_mendez2.6a632f53.png"
+  },
+  {
+    "revision": "fea5ecc2358b0e830e8a8054db645fb5",
+    "url": "/tuki/static/media/sebastian_rodriguez4.fea5ecc2.png"
+  },
+  {
+    "revision": "90f6dbb6805327c08ed184d1d282086f",
+    "url": "/tuki/static/media/tomas_mejia4.90f6dbb6.png"
+  },
+  {
+    "revision": "1c311b5a1247515798a43f3d1042772e",
+    "url": "/tuki/static/media/unknown2.1c311b5a.png"
+  }
+]);
