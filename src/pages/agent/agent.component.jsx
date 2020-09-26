4a00e519dbd9c8f7ece7156fde364736d53f9dff
Jesus Rojas <jesusarvelais1000@hotmail.com>
2020-09-26 12:54:03 -0700

    fixing arwesSounds with gh-pages relation

--- src/pages/agent/agent.component.jsx
+++ src/pages/agent/agent.component.jsx
@@ -17,7 +17,7 @@ const Agent = (props) => {
         changeAgentsAsync();
     }, [])
 
-    const { name, fullName, status, location, specialty, languages, imageUrl } = agent;
+    const { name, fullName, status, location, specialty, affiliation, languages, imageLargeUrl } = agent;
     
     return (
       <div>
@@ -25,68 +25,74 @@ const Agent = (props) => {
         <div className="characterPageView">
           <Project animate header={`TUKI AGENT PROFILE`}>
             <div className="characterInfo">
-              <p className="characterBio">
-                <Words animate>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis laboris nisi ut aliquip ex. Duis
-                  aute irure. Consectetur adipisicing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                  minim veniam, quis dorime.
-                </Words>
-              </p>
+              <div className="characterSpecifics">
+                <p>
+                  <Words style={{ fontWeight: 700 }} animate>
+                    Status:
+                  </Words>{" "}
+                  <Words animate layer={statusColor(status)}>
+                    {status}
+                  </Words>
+                </p>
+                <p>
+                  <Words style={{ fontWeight: 700 }} animate>
+                    Birth Name:
+                  </Words>{" "}
+                  <Words animate>{fullName}</Words>
+                </p>
+                <p>
+                  <Words style={{ fontWeight: 700 }} animate>
+                    Alias:
+                  </Words>{" "}
+                  <Words animate>{name}</Words>
+                </p>
+                <p>
+                  <Words style={{ fontWeight: 700 }} animate>
+                    Location:
+                  </Words>{" "}
+                  <Words animate>{location}</Words>
+                </p>
+                <p>
+                  <Words style={{ fontWeight: 700 }} animate>
+                    Specialty:
+                  </Words>{" "}
+                  <Words animate>{specialty}</Words>
+                </p>
+                <p>
+                  <Words style={{ fontWeight: 700 }} animate>
+                    Affiliation:
+                  </Words>{" "}
+                  <Words animate>{affiliation}</Words>
+                </p>
+                <p>
+                  <Words style={{ fontWeight: 700 }} animate>
+                    Language:
+                  </Words>{" "}
+                  <Words animate>{languages}</Words>
+                </p>
+              </div>
               <Image
                 className="characterImage"
-                resources={require(`../../images/${checkIfLoaded(imageUrl)}`)}
+                resources={require(`../../images/${checkIfLoaded(imageLargeUrl)}`)}
               />
             </div>
-            <div className="characterSpecifics">
-              <p>
-                <Words style={{ fontWeight: 700 }} animate>
-                  Status:
-                </Words>{" "}
-                <Words animate layer={statusColor(status)}>
-                  {status}
-                </Words>
-              </p>
-              <p>
-                <Words style={{ fontWeight: 700 }} animate>
-                  Birth Name:
-                </Words>{" "}
-                <Words animate>{fullName}</Words>
-              </p>
-              <p>
-                <Words style={{ fontWeight: 700 }} animate>
-                  Alias:
-                </Words>{" "}
-                <Words animate>{name}</Words>
-              </p>
-              <p>
-                <Words style={{ fontWeight: 700 }} animate>
-                  Location:
-                </Words>{" "}
-                <Words animate>{location}</Words>
-              </p>
-              <p>
-                <Words style={{ fontWeight: 700 }} animate>
-                  Specialty:
-                </Words>{" "}
-                <Words animate>{specialty}</Words>
-              </p>
-              <p>
-                <Words style={{ fontWeight: 700 }} animate>
-                  Language:
-                </Words>{" "}
-                <Words animate>{languages}</Words>
-              </p>
-              <Content>
-                <Link to="/agents">
-                  <p className="go-back-page">
-                    <a>Go back to agents</a>
-                  </p>
-                </Link>
-              </Content>
-            </div>
+            <p className="characterBio">
+              <Words animate>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis laboris nisi ut aliquip ex. Duis aute
+                irure. Consectetur adipisicing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                veniam, quis dorime.
+              </Words>
+            </p>
+            <Content>
+              <Link to="/agents">
+                <p className="go-back-page">
+                  <a>Go back to agents</a>
+                </p>
+              </Link>
+            </Content>
           </Project>
         </div>
       </div>
